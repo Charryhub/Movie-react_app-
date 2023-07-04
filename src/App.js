d360144f214db3dc0b67246146d5901242067039
Charity <root@Harrisons-MacBook-Pro.local>
2023-07-04 23:41:52 +0300

    added https

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 
 import "./App.css";
 import SearchIcon from "./search.svg";
-const API_URL = "http://www.omdbapi.com?apikey=8f587d38";
+const API_URL = "https://www.omdbapi.com?apikey=8f587d38";
 // const movie1 = {
 //   Title: "Spiderman",
 //   Year: "1990",
